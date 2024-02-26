8a0ea7edcf78d8189a31000548556793376e4e35
David <65638264+jakStrick@users.noreply.github.com>
2024-02-25 17:41:07 -0800

    RenamedProject

---
+++ TGC_SuggesterApp/TGC_SuggesterApp/TGC_SuggesterApp/TCG_SuggesterApp.cs
@@ -0,0 +1,17 @@
+﻿namespace TGC_ConjectureSuggesterApp;
+
+internal class FormCosmicSocialStateBase
+{
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+}
